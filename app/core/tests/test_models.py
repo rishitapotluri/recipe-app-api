f60874475b87e8785068f89eb99f16beca2d6112
PratikshaKotkar <pratiksha.kotkar@audetemi.com>
2021-02-07 19:32:44 +0530

    custom authentication setup

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,14 +1,34 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+
 class ModelTests(TestCase):
     def test_create_user_with_email_successful(self):
         """Test for creating a new user with an email"""
         email = 'test@gmail.com'
         password = 'test123'
-        user=get_user_model().objects.create_user(
-            email = email,
-            password = password
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
         )
-        self.assertEqual(user.email,email)
-        self.assertTrue(user.check_password,password)
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password, password)
+
+    def test_new_user_email_normalized(self):
+        """Chheck if email is normalized"""
+        email = 'test@gmail.com'
+        user = get_user_model().objects.create_user(email, 'test123')
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """Check if creating a new user with no email returns error."""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'test123')
+
+    def test_create_new_super_user(self):
+        user = get_user_model().objects.create_superuser(
+            'test@gmail.com',
+            'test123',
+        )
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
